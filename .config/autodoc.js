236c8e27c83d19147556334fa1f617ea5a537563
Romain Barissat <romain-noreply@barissat.com>
2020-09-24 13:07:02 +0700

    Chore: Tweak autodoc

--- .config/autodoc.js
+++ .config/autodoc.js
@@ -10,7 +10,7 @@ const table = [];
 Object.keys(yml.inputs).forEach((input) => {
   table.push({
     input,
-    required: yml.inputs[input] && yml.inputs[input].required ? yml.inputs[input].required : false,
+    required: yml.inputs[input] && yml.inputs[input].required ? "yes" : "",
     default: yml.inputs[input] && yml.inputs[input].default ? yml.inputs[input].default : "",
     description: yml.inputs[input] && yml.inputs[input].description ? yml.inputs[input].description : "",
   });
