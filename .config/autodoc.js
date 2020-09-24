0ed7563c74f41a371967df68cfe4c404b204f089
Romain Barissat <romain-noreply@barissat.com>
2020-09-24 09:47:45 +0700

    Chore: Add auto doc for inputs on pre-commit

---
+++ .config/autodoc.js
@@ -0,0 +1,19 @@
+const YAML = require("yaml");
+const tablemark = require("tablemark");
+const fs = require("fs");
+
+const file = fs.readFileSync("./action.yml", "utf8");
+
+const yml = YAML.parse(file);
+const table = [];
+
+Object.keys(yml.inputs).forEach((input) => {
+  table.push({
+    input,
+    required: yml.inputs[input] && yml.inputs[input].required ? yml.inputs[input].required : false,
+    default: yml.inputs[input] && yml.inputs[input].default ? yml.inputs[input].default : "",
+    description: yml.inputs[input] && yml.inputs[input].description ? yml.inputs[input].description : "",
+  });
+});
+
+fs.writeFileSync("./docs/inputs.md", tablemark(table));
---
+++ .config/commit-docs.sh
@@ -0,0 +1,26 @@
+# This script is used for repos that need to commit a build folder (eg. Github actions)
+
+# Add more sources/destinations by separating them by spaces (ie. source="src lib")
+source="action.yml"
+build="docs/inputs.md"
+
+# If some changes in $source are staged
+if [[ $(git status --porcelain $source | egrep '^M') ]]
+
+then
+
+  # Run build command
+  echo "Generating $build..."
+  node .config/autodoc.js
+  
+  # If this generated unstaged changes in $build folder(s)
+  if [[ $(git status --porcelain $build | egrep '^(([M ]M)|\?\?)') ]]
+
+  then
+
+    git add $build
+    echo "ℹ️  Added $build to your commit"
+    
+  fi
+
+fi
